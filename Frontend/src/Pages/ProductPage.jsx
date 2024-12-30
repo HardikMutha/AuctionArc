d027216ffe727ec1587807b9a689de692f5d19dc
arymehta <amehta350@gmail.com>
2024-12-30 18:24:03 +0530

    updated token validation, implemented functioning product page, makde productCard into a link to product page

--- Frontend/src/Pages/ProductPage.jsx
+++ Frontend/src/Pages/ProductPage.jsx
@@ -1,9 +1,9 @@
 import { useParams } from "react-router";
 import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
-import { 
-  Container, 
-  Typography, 
+import {
+  Container,
+  Typography,
   Box,
   Button,
   Stack,
@@ -11,19 +11,38 @@ import {
   Paper,
   Grid2,
   useTheme,
-  useMediaQuery 
+  useMediaQuery,
 } from "@mui/material";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import GavelIcon from "@mui/icons-material/Gavel";
 import TimerIcon from "@mui/icons-material/Timer";
 import LocalOfferIcon from "@mui/icons-material/LocalOffer";
-import { format } from 'date-fns';
+import axios from "axios";
+import ProductCard from "../components/ProductCard";
+
+// Click handler function
 
 export default function ProductPage() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
+  const [similarProducts, setSimilarProducts] = useState(null);
   const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
+  // Adds the product to current user's wishlist
+  const handleWishListClick = async () => {
+    try {
+      const response = await axios.post(
+        `http://localhost:3000/wish-list/add-to-wishlist/${id}`,
+        { id: id },
+        // required for token validat
+        { withCredentials: true }
+      );
+      console.log("Item added to wishlist:", response.data);
+    } catch (error) {
+      console.error("Error adding item to wishlist:", error);
+    }
+  };
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -35,23 +54,35 @@ export default function ProductPage() {
         console.error("Error Fetching Product : ", error);
       }
     };
+    const getSimilarProducts = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/get-similar-products/${id}`,
+          { withCredentials: true }
+        );
+        setSimilarProducts(response.data);
+      } catch (error) {
+        console.log("Error getting similar products : ", error);
+      }
+    };
     fetchProduct();
+    getSimilarProducts();
   }, [id]);
 
   return (
     <>
       <Navbar />
-      <Container 
-        maxWidth="lg" 
-        sx={{ 
+      <Container
+        maxWidth="lg"
+        sx={{
           pt: { xs: 8, md: 12 },
           pb: 8,
-          marginTop : 10
+          marginTop: 10,
         }}
       >
-        <Grid2 
-          container 
-          spacing={{ xs: 2, md: 4 }} 
+        <Grid2
+          container
+          spacing={{ xs: 2, md: 4 }}
           alignItems="center"
           justifyContent="center"
         >
@@ -61,21 +92,24 @@ export default function ProductPage() {
               elevation={3}
               sx={{
                 borderRadius: 4,
-                overflow: 'hidden',
-                aspectRatio: '1/1',
-                width: '100%',
-                maxWidth: { xs: '100%', md: 500 },
-                mx: 'auto'
+                overflow: "hidden",
+                aspectRatio: "1/1",
+                width: "100%",
+                maxWidth: { xs: "100%", md: 500 },
+                mx: "auto",
               }}
             >
               <Box
                 component="img"
-                src= {product?.images[0] || "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"}
+                src={
+                  product?.images[0] ||
+                  "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
+                }
                 alt="product"
                 sx={{
-                  width: '100%',
-                  height: '100%',
-                  objectFit: 'cover'
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
                 }}
               />
             </Paper>
@@ -87,43 +121,40 @@ export default function ProductPage() {
               sx={{
                 borderRadius: 4,
                 p: { xs: 3, md: 4 },
-                height: '100%',
-                maxWidth: { xs: '100%', md: 500 },
-                mx: 'auto'
+                height: "100%",
+                maxWidth: { xs: "100%", md: 500 },
+                mx: "auto",
               }}
             >
               <Stack spacing={3}>
                 {/* Title */}
-                <Typography 
-                  variant={isMobile ? "h4" : "h3"} 
-                  fontWeight="500"
-                >
+                <Typography variant={isMobile ? "h4" : "h3"} fontWeight="500">
                   {product?.name || "Product Name"}
                 </Typography>
 
                 {/* Tags */}
-                <Stack 
-                  direction="row" 
-                  spacing={1} 
-                  flexWrap="wrap" 
+                <Stack
+                  direction="row"
+                  spacing={1}
+                  flexWrap="wrap"
                   sx={{ gap: 1 }}
                 >
                   {/* {['Fashion', 'Formals', 'Electronics'].map((tag) => ( */}
-                    <Chip
-                      key={product?.category}
-                      icon={<LocalOfferIcon />}
-                      label={product?.category}
-                      variant="outlined"
-                      size={isMobile ? "small" : "medium"}
-                    />
+                  <Chip
+                    key={product?.category}
+                    icon={<LocalOfferIcon />}
+                    label={product?.category}
+                    variant="outlined"
+                    size={isMobile ? "small" : "medium"}
+                  />
                   {/* ))} */}
                 </Stack>
 
                 {/* Price and Description */}
                 <Box>
-                  <Typography 
-                    variant={isMobile ? "h5" : "h4"} 
-                    fontWeight="500" 
+                  <Typography
+                    variant={isMobile ? "h5" : "h4"}
+                    fontWeight="500"
                     gutterBottom
                   >
                     ${product?.listingPrice || "0.00"}
@@ -134,10 +165,10 @@ export default function ProductPage() {
                 </Box>
 
                 {/* Action Buttons */}
-                <Stack 
-                  direction={{ xs: 'column', sm: 'row' }} 
+                <Stack
+                  direction={{ xs: "column", sm: "row" }}
                   spacing={2}
-                  sx={{ width: '100%' }}
+                  sx={{ width: "100%" }}
                 >
                   <Button
                     variant="contained"
@@ -146,13 +177,14 @@ export default function ProductPage() {
                     fullWidth
                     sx={{
                       borderRadius: 8,
-                      bgcolor: '#E43A36',
-                      '&:hover': {
-                        bgcolor: '#D32F2F',
-                        transform: 'scale(1.02)',
-                        transition: 'all 0.3s'
-                      }
+                      bgcolor: "#E43A36",
+                      "&:hover": {
+                        bgcolor: "#D32F2F",
+                        transform: "scale(1.02)",
+                        transition: "all 0.3s",
+                      },
                     }}
+                    onClick={handleWishListClick} // Attach the click handler
                   >
                     Add to Wishlist
                   </Button>
@@ -163,14 +195,14 @@ export default function ProductPage() {
                     fullWidth
                     sx={{
                       borderRadius: 8,
-                      bgcolor: '#FFD700',
-                      color: 'black',
-                      '&:hover': {
-                        bgcolor: 'black',
-                        color: 'white',
-                        transform: 'scale(1.02)',
-                        transition: 'all 0.3s'
-                      }
+                      bgcolor: "#FFD700",
+                      color: "black",
+                      "&:hover": {
+                        bgcolor: "black",
+                        color: "white",
+                        transform: "scale(1.02)",
+                        transition: "all 0.3s",
+                      },
                     }}
                   >
                     Place Bid!
@@ -178,17 +210,17 @@ export default function ProductPage() {
                 </Stack>
 
                 {/* Timer */}
-                <Box 
-                  sx={{ 
-                    display: 'flex', 
-                    alignItems: 'center', 
+                <Box
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
                     gap: 1,
-                    color: 'text.secondary'
+                    color: "text.secondary",
                   }}
                 >
                   <TimerIcon />
                   <Typography>
-                    Auction live till:  {product?.duration.slice(0, 10)|| "N/A"}
+                    Auction live till: {product?.duration.slice(0, 10) || "N/A"}
                   </Typography>
                 </Box>
               </Stack>
@@ -196,6 +228,65 @@ export default function ProductPage() {
           </Grid2>
         </Grid2>
       </Container>
+
+      <Box
+        sx={{
+          p: "20px",
+          display: "flex", // Use flexbox for centering
+          justifyContent: "center", // Center horizontally
+          alignItems: "center", // Center vertically
+          flexDirection: "column", // Stack items vertically
+          gap: 8,
+          mt: 2,
+        }}
+      >
+        <Typography
+          variant={isMobile ? "h5" : "h6"}
+          fontWeight="500"
+          color="text.secondary"
+          sx={{ px: "20px" }}
+        >
+          Products similar to "{product?.name}" ...
+        </Typography>
+        <hr
+          style={{
+            color: "black", // This will not affect the <hr> color
+            height: "1px", // Increase the thickness
+            backgroundColor: "gray", // Set the color of the <hr>
+            border: "none", // Remove the default border
+            width: "100%",
+          }}
+        ></hr>
+        {/* Check if similarProducts is available */}
+        {similarProducts && similarProducts.length > 0 ? (
+          <Box
+            sx={{
+              display: "grid",
+              gridTemplateColumns: {
+                xs: "repeat(1, 1fr)", // 1 column for extra small screens
+                sm: "repeat(2, 1fr)", // 2 columns for small screens
+                md: "repeat(3, 1fr)", // 3 columns for medium screens
+                lg: "repeat(3, 1fr)", // 4 columns for large screens
+              },
+              gap: 8,
+            }}
+          >
+            {similarProducts.map((similarProduct) => (
+              <ProductCard productDetails={similarProduct} />
+            ))}
+          </Box>
+        ) : (
+          <Box sx={{ textAlign: "left", width: "100%" }}>
+            <Typography
+              variant={isMobile ? "h5" : "h4"}
+              fontWeight="500"
+              gutterBottom
+            >
+              No similar product found
+            </Typography>
+          </Box>
+        )}
+      </Box>
     </>
   );
 }
--- Frontend/src/components/ProductCard.jsx
+++ Frontend/src/components/ProductCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"; // Import Link from React Router
 import { Container, Typography } from "@mui/material";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import { useState } from "react";
@@ -6,59 +7,75 @@ function ProductCard({ productDetails }) {
     const [isHovered, setIsHovered] = useState(false);
 
     return (
-        <div
-            style={{
-                borderRadius: "20px",
-                width: "350px",
-                height: "400px",
-                border: "1px solid #ccc",
-                boxShadow: "10px 10px 5px #ccc",
-                display: "flex",
-                flexDirection: "column",
-                overflow: "hidden", // Ensures the content respects the rounded corners
-                transition: "all 0.3s ease",
-                backgroundColor: isHovered ? "#f0f0f0" : "white", // Change background color on hover
-                transform: isHovered ? "scale(1.02)" : "scale(1)",
-            }}
-            onMouseEnter={() => setIsHovered(true)} // Set hover state to true
-            onMouseLeave={() => setIsHovered(false)} // Set hover state to false    
-            key = {productDetails._id} 
+        <Link
+            to={`/products/${productDetails._id}`} // Make the card a link
+            style={{ textDecoration: "none", color: "inherit" }} // Remove underline and inherit text color
         >
-            <div style={{ width : "100%", height: "80%", position: "relative", display : "flex", justifyContent : "center"}}>
-                <img
+            <div
+                style={{
+                    borderRadius: "20px",
+                    width: "350px",
+                    height: "400px",
+                    border: "1px solid #ccc",
+                    boxShadow: "10px 10px 5px #ccc",
+                    display: "flex",
+                    flexDirection: "column",
+                    overflow: "hidden", // Ensures the content respects the rounded corners
+                    transition: "all 0.3s ease",
+                    backgroundColor: isHovered ? "#f0f0f0" : "white", // Change background color on hover
+                    transform: isHovered ? "scale(1.02)" : "scale(1)",
+                }}
+                onMouseEnter={() => setIsHovered(true)} // Set hover state to true
+                onMouseLeave={() => setIsHovered(false)} // Set hover state to false
+                key={productDetails._id}
+            >
+                <div
                     style={{
-                        objectFit: "cover",
                         width: "100%",
-                        height: "100%",
-                        borderBottomLeftRadius: "20px",
-                        borderBottomRightRadius: "20px", // Ensure image respects rounded corners
+                        height: "80%",
+                        position: "relative",
+                        display: "flex",
+                        justifyContent: "center",
                     }}
-                    src= {productDetails.images[0] || "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png" }
-                    alt="Product"
-                />
+                >
+                    <img
+                        style={{
+                            objectFit: "cover",
+                            width: "100%",
+                            height: "100%",
+                            borderBottomLeftRadius: "20px",
+                            borderBottomRightRadius: "20px", // Ensure image respects rounded corners
+                        }}
+                        src={
+                            productDetails.images[0] ||
+                            "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png"
+                        }
+                        alt="Product"
+                    />
 
-                <FavoriteIcon
-                    sx={{
-                        position: "absolute",
-                        top: "10px",
-                        right: "10px",
-                        color: "red",
-                        transition: "all 0.3s ease",
-                        scale: isHovered ? "1.05" : "1.00",
-                        transform: isHovered ? "rotate(-10deg)" : "rotate(0deg)", // Rotate on hover
-                    }}
-                />
+                    <FavoriteIcon
+                        sx={{
+                            position: "absolute",
+                            top: "10px",
+                            right: "10px",
+                            color: "red",
+                            transition: "all 0.3s ease",
+                            scale: isHovered ? "1.05" : "1.00",
+                            transform: isHovered ? "rotate(-10deg)" : "rotate(0deg)", // Rotate on hover
+                        }}
+                    />
+                </div>
+
+                <Container sx={{ padding: 2 }}>
+                    <Typography variant="h5" sx={{ color: "black" }}>
+                        {productDetails.name}
+                    </Typography>
+                    <Typography variant="h6" sx={{ color: "gray" }}>
+                        {"$ " + productDetails.listingPrice}
+                    </Typography>
+                </Container>
             </div>
-
-            <Container sx={{ padding: 2 }}>
-                <Typography variant="h5" sx={{ color: "black" }}>
-                    {productDetails.name}
-                </Typography>
-                <Typography variant="h6" sx={{ color: "gray" }}>
-                    {"$ " + productDetails.listingPrice}
-                </Typography>
-            </Container>
-        </div>
+        </Link>
     );
 }
 
