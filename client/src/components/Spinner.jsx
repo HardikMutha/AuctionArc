3e33661d3529f6ba0a882139b7c687318e2d6a50
HardikMutha <hardikmutha2005@gmail.com>
2025-05-27 13:03:03 +0530

    Fix - Spinner and Update vercel envs

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -1,21 +1,16 @@
-import ClipLoader from "react-spinners/ClipLoader";
-
-function Spinner(props) {
-  const color = "#000000";
+/* eslint-disable react/prop-types */
+const Spinner = ({ size = 60 }) => {
   return (
-    <div
-      className="sweet-loading"
-      style={{ display: "flex", justifyContent: "center" }} // Center the spinner
-    >
-      <ClipLoader
-        color={color}
-        // eslint-disable-next-line react/prop-types
-        size={props.size}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      />
+    <div className="fixed inset-0 bg-white bg-opacity-90 flex items-center justify-center z-50">
+      <div className="text-center">
+        <div
+          className="animate-spin rounded-full border-4 border-gray-200 border-t-blue-500"
+          style={{ width: size, height: size }}
+        ></div>
+        <p className="mt-4 text-gray-600 font-medium">Loading...</p>
+      </div>
     </div>
   );
-}
+};
 
 export default Spinner;
